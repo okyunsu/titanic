e4a5d3bf312ea83d7332fe1f4f3d0d7823b3913d
okyunsu <okyunsu@gmail.com>
2025-02-14 18:02:08 +0900

    25/02/14

--- com/okyunsu/models/titanic/titanic_service.py
+++ com/okyunsu/models/titanic/titanic_service.py
@@ -1,6 +1,27 @@
 from com.okyunsu.models.titanic.dataset import Dataset
 import pandas as pd
 
+
+"""
+PassengerId  고객ID,
+Survived 생존여부,
+Pclass 승선권 1 = 1등석, 2 = 2등석, 3 = 3등석,
+Name,
+Gender,
+Age,
+SibSp 동반한 형제, 자매, 배우자,
+Parch 동반한 부모, 자식,
+Ticket 티켓번호,
+Fare 요금,
+Cabin 객실번호,
+Embarked 승선한 항구명 C = 쉐브루, Q = 퀸즈타운, S = 사우스햄튼
+print(f'결정트리 활용한 검증 정확도 {None}')
+print(f'랜덤포레스트 활용한 검증 정확도 {None}')
+print(f'나이브베이즈 활용한 검증 정확도 {None}')
+print(f'KNN 활용한 검증 정확도 {None}')
+print(f'SVM 활용한 검증 정확도 {None}')
+"""
+
 class TitanicService: 
 
     dataset = Dataset()
@@ -10,3 +31,27 @@ class TitanicService:
         this.context = "C:\\Users\\bitcamp\\Documents\\Titanic\\com\\okyunsu\\datas\\titanic\\"
         this.fname = fname
         return pd.read_csv(this.context + this.fname) 
+    
+    @staticmethod
+    def pclass_ordinal():
+        pass
+
+    @staticmethod
+    def gender_nominal():
+        pass
+ 
+    @staticmethod
+    def age_ordinal():
+        pass
+  
+    @staticmethod
+    def sibSp_ordinal():
+        pass
+ 
+    @staticmethod
+    def sibSp_ordinal():
+        pass
+  
+    @staticmethod
+    def embarked_nominal():
+        pass
